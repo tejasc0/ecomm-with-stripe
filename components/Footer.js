516de8de6031b3f77a18172e9d34cd213f538d32
tejasc0 <tejasc888@gmail.com>
2022-04-30 11:54:36 +0000

    added cart

--- components/Footer.js
+++ components/Footer.js
@@ -1,8 +1,14 @@
-
+import { AiFillInstagram, AiOutlineTwitter } from 'react-icons/ai';
 
 const Footer = () => {
   return (
-    <div>Footer</div>
+    <div className="footer-container">
+      <p>2022 JSM Headphones All rights reserverd</p>
+      <p className="icons">
+        <AiFillInstagram />
+        <AiOutlineTwitter />
+      </p>
+    </div>
   )
 }
 
